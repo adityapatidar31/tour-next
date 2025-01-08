f8cc1bdbf34f308c9f107e0083b6b97caffb5ade
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-08 19:26:35 +0530

    create a get route 'api/v1/tours'

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/api/v1/tours", (req, res) => {});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      tours,
+    },
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on the port ${PORT}...`);

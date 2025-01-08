cf1baa2d527770d2dadf08bb113f84d48de0c086
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-08 20:19:09 +0530

    refactor the code remove code from the route to handlers

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     result: tours.length,
@@ -18,9 +18,9 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.get("/api/v1/tours/:id", (req, res) => {
+const getSingleTour = (req, res) => {
   const id = Number(req.params.id);
   const tour = tours.find((tour) => tour.id === id);
   res.status(200).json({
@@ -29,9 +29,9 @@ app.get("/api/v1/tours/:id", (req, res) => {
       tour,
     },
   });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -48,16 +48,31 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
   const id = Number(req.params.id);
   const tour = tours.find((tour) => tour.id === id);
   res.status(200).json({
     status: "success",
     message: "<Tour Updated>",
   });
-});
+};
+
+const deleteTour = (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((tour) => tour.id === id);
+  res.status(204).json({
+    status: "success",
+    data: {},
+  });
+};
+
+app.get("/api/v1/tours", getAllTours);
+app.get("/api/v1/tours/:id", getSingleTour);
+app.post("/api/v1/tours", createTour);
+app.patch("/api/v1/tours/:id", updateTour);
+app.delete("/api/v1/tours/:id", deleteTour);
 
 app.listen(PORT, () => {
   console.log(`App is running on the port ${PORT}...`);

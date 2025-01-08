d8ba436e42d5f6bfabe6bb3d3a6cf1179ec7f5ad
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-08 19:55:00 +0530

    add route to post  new tour '/api/v1/tours'

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const PORT = 3000;
 
 const fs = require("fs");
 const express = require("express");
-
 const app = express();
+
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -17,6 +18,25 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(PORT, () => {

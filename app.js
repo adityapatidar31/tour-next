db97c946ad16e22a4185d1b674281bff29b38b1c
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-08 19:23:13 +0530

    add dev-data to the project

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const PORT = 3000;
 
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Hello From the server",
-    app: tourNext,
-  });
-});
+app.get("/api/v1/tours", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`App is running on the port ${PORT}...`);

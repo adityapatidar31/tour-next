09666a33850030311eb374d51960a709f56d9695
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-09 18:27:47 +0530

    create createTour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,11 +22,18 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = async (req, res) => {
-  const newTour = await Tour.create(req.body);
-  res.status(201).json({
-    status: "success",
-    data: newTour,
-  });
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: newTour,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {

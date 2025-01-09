9b16d72a61b91ad641a3840d5f49a0af4cff7ef3
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-09 20:46:15 +0530

    add pagination in the getAllTours controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,6 +27,15 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select("-__v");
     }
+
+    // 5. Pagination
+    if (req.query.page) {
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 3;
+      const skip = (page - 1) * limit;
+      query = query.skip(skip).limit(limit);
+    }
+
     // Execute Query
     const tours = await query;
     res.status(200).json({

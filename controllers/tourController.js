a95da188e26fe8332c1690db8bc0a0c2ba6ee3d2
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-09 12:50:02 +0530

    fix bug according to eslint

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,8 @@
+/* eslint-disable prefer-object-spread */
 const fs = require("fs");
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
 exports.checkID = (req, res, next, val) => {
@@ -58,20 +59,20 @@ exports.createTour = (req, res) => {
 
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
-
   tours.push(newTour);
 
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
+      console.log(err);
       res.status(201).json({
         status: "success",
         data: {
           tour: newTour,
         },
       });
-    }
+    },
   );
 };
 

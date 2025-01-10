40588e410dcab9eb4b4723c44fe121633ccb0d81
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-10 17:10:56 +0530

    add handing for Unhandled Routes

--- app.js
+++ app.js
@@ -23,4 +23,12 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// 4) Unhandled Routes
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on the server`,
+  });
+});
+
 module.exports = app;

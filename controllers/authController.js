1a8b2cef05804c24948d4b35815e6f4ee779f8e8
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-11 14:31:55 +0530

    complete the login controller and reuse code signToken

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,12 @@ const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
+function signToken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
+
 exports.signUp = catchAsync(async (req, res) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -12,9 +18,7 @@ exports.signUp = catchAsync(async (req, res) => {
     photo: req.body.photo,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: "success",
@@ -32,10 +36,13 @@ exports.login = catchAsync(async (req, res, next) => {
     next(new AppError("Please Provide email and password", 400));
   }
   // 2. Check if user exists and password is correct
-  // const user=User.find({_id:})
-  // if(!user)
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    next(new AppError("Invalid Credentials", 401));
+  }
   // 3. if everything ok, send token to client
-  const token = "";
+  const token = signToken(user._id);
   res.status(200).json({
     status: "success",
     token,

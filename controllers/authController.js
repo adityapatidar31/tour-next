20865654773a725fc47f2ab9a39c0a9a45ff58ba
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-11 14:09:52 +0530

    create a half controller for login controller send responce and check email and password in req

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.signUp = catchAsync(async (req, res) => {
   const newUser = await User.create({
@@ -21,5 +22,22 @@ exports.signUp = catchAsync(async (req, res) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // 1. Check if email and password not exist
+  if (!email || !password) {
+    next(new AppError("Please Provide email and password", 400));
+  }
+  // 2. Check if user exists and password is correct
+  // const user=User.find({_id:})
+  // if(!user)
+  // 3. if everything ok, send token to client
+  const token = "";
+  res.status(200).json({
+    status: "success",
+    token,
   });
 });

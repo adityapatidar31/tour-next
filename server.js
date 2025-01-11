23ea08e754b703859080555fb3f7d4de67c52b3b
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-11 10:49:33 +0530

    handle unhandledRejection and close the server

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDELED REJECTION!!!!  SHUTTING DOWN ....");
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1));
 });

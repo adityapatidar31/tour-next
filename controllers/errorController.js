2b7ab940d432e050922c36de85795e959d1e7b2a
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-11 20:51:57 +0530

    add error handling for the error JsonWebTokenError

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,9 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError("Invalid token. Please log in again!", 401);
+
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
   const message = `Duplicate field value: ${value}. Please use another value!`;
@@ -56,6 +59,8 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+
     sendErrorProd(error, res);
   }
 };

f0cf1d7f6effed085f3953df24e0c5d2f5c95101
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-12 19:37:25 +0530

    add token in the cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,17 @@ function signToken(id) {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  res.cookie("jwt", token, cookieOptions);
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
   user.password = undefined;
   res.status(statusCode).json({
     status: "success",

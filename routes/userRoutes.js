6d9a67ecae1c3c6404d70dd23c9e8de53110b249
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-12 14:22:38 +0530

    fix the bug of JWT token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,7 @@ router.patch("/updateMe", authController.protect, userController.updateMe);
 router.delete("/deleteMe", authController.protect, userController.deleteMe);
 
 router.patch(
-  "updateMyPassword",
+  "/updateMyPassword",
   authController.protect,
   authController.updatePassword,
 );

0f116690babc7c4f532ef68341cb520e4e90b202
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-14 13:12:17 +0530

    add hpp to save app from parameter pollution attacK

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -45,6 +46,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter Pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsAverage",
+      "maxGroupSize",
+      "ratingsQuantity",
+      "difficulty",
+      "price",
+    ],
+  }),
+);
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();

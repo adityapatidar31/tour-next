6d4605bae83f96f3b31e88123b1a535715d9ec87
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-16 00:29:32 +0530

    fix bug of req.body.user in setTourUserIds

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -21,7 +21,7 @@ exports.getAllReviews = catchAsync(async (req, res) => {
 
 exports.setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.tour = req.user.id;
+  if (!req.body.user) req.body.user = req.user.id;
   next();
 };
 

5c1ca209101a56baf21d0ba4500cef8cbdd803ea
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-16 14:22:10 +0530

    create a '/me' route to get info about current user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,10 @@ router.patch("/updateMe", authController.protect, userController.updateMe);
 
 router.delete("/deleteMe", authController.protect, userController.deleteMe);
 
+router
+  .route("/me")
+  .get(authController.protect, userController.getMe, userController.getUser);
+
 router.patch(
   "/updateMyPassword",
   authController.protect,

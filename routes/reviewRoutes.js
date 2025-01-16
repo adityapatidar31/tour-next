206a1e5aa55324a63e2e00148633f483f21e45a4
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-16 14:30:25 +0530

    add authentication and authorization in the reviews routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,11 +6,12 @@ const router = express.Router({ mergeParams: true });
 // POST /tour/:tourId/reviews
 // POST /reviews
 
+router.use(authController.protect);
+
 router
   .route("/")
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo("user"),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -19,7 +20,13 @@ router
 router
   .route("/:id")
   .get(reviewController.getReview)
-  .delete(authController.restrictTo("admin"), reviewController.deleteReview)
-  .patch(reviewController.updateReview);
+  .patch(
+    authController.restrictTo("user", "admin"),
+    reviewController.updateReview,
+  )
+  .delete(
+    authController.restrictTo("user", "admin"),
+    reviewController.deleteReview,
+  );
 
 module.exports = router;

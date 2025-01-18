ebf99c729d5d8fc569903a0686a3e62f69f8a250
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-18 23:34:51 +0530

    refactor the code create viewsController and viewRoutes.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const userRouter = require("./routes/userRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRoutes = require("./routes/viewRoutes");
 
 const app = express();
 
@@ -74,22 +75,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("base");
-});
-
-app.get("/overview", (req, res) => {
-  res.status(200).render("overview", {
-    title: "The forest Hiker Tour",
-  });
-});
-
-app.tour("/tour", (req, res) => {
-  res.status(200).render("tour", {
-    title: "The Forest Hiker Tour",
-  });
-});
-
+app.use("/", viewRoutes);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);

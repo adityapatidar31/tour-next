6d778460e5100de31f0f00de3388f2a011714849
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-02 03:57:54 +0530

    add react-tostify and setup both react-toast and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ToastContainer } from "react-toastify";
+
 import Hero from "./components/components/Hero";
 import Product from "./components/components/pages/product/ProductPage";
 import { Outlet } from "react-router-dom";
@@ -6,6 +10,9 @@ import Navbar from "./components/components/navbar/Navbar";
 import HomePage from "./components/components/HomePage";
 import LoginPage from "./components/components/pages/login/LoginPage";
 import RegisterPage from "./components/components/pages/register/registerPage";
+import { useEffect, useState } from "react";
+
+const queryClient = new QueryClient();
 
 function Layout() {
   return (
@@ -19,19 +26,39 @@ function Layout() {
 }
 
 export default function App() {
+  const [theme, setTheme] = useState("dark");
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme") as "light" | "dark";
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/home" element={<Layout />}>
-          {/* The HomePage is the default for /home */}
-          <Route path="" element={<HomePage />} />
-          {/* The dynamic route is defined as :id */}
-          <Route path=":id" element={<Product />} />
-        </Route>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick={false}
+        draggable
+        theme={theme}
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/home" element={<Layout />}>
+            {/* The HomePage is the default for /home */}
+            <Route path="" element={<HomePage />} />
+            {/* The dynamic route is defined as :id */}
+            <Route path=":id" element={<Product />} />
+          </Route>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }

81e446b6274a69e6ee33b8dc56837cccb8de2f4b
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-03 04:35:06 +0530

    fix the bug of the protect route replace || with &&

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,7 +67,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   // 1. Getting token and check of it's there
   let token;
   if (
-    req.headers.authorization ||
+    req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];

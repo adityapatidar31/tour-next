8309c52820d89d724b32df651b9e09a311736a8f
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-04 20:52:10 +0530

    correct the ui of the filter component

--- src/components/components/Filter.tsx
+++ src/components/components/Filter.tsx
@@ -59,81 +59,83 @@ export default function FilterComponent() {
     navigate("/home", { replace: true });
   }
   return (
-    <Card className="p-4 w-full  mx-auto space-y-4 bg-background">
-      <div className="flex sm:flex-row flex-col">
-        {/* Difficulty Filter */}
-        <div className="w-full">
-          <h3 className="text-lg font-semibold">Difficulty</h3>
-          <div className="flex gap-3 mt-2">
-            {difficulty.map((diff) => (
-              <label key={diff} className="flex items-center gap-2">
-                <Checkbox
-                  checked={selectedDifficulty.includes(diff)}
-                  onCheckedChange={() => handleDifficultyChange(diff)}
-                />
-                {diff}
-              </label>
-            ))}
-          </div>
-        </div>
-        {/* Sort Filter */}
-        <div className="w-96">
-          <h3 className="text-lg font-semibold">Sort by</h3>
-          <Select value={sort} onValueChange={setSort}>
-            <SelectTrigger className="w-full">
-              {sortOptions.find((s) => s.value === sort)?.label}
-            </SelectTrigger>
-            <SelectContent>
-              {sortOptions.map((option) => (
-                <SelectItem key={option.value} value={option.value}>
-                  {option.label}
-                </SelectItem>
+    <div className="mx-1 sm:mx-3 my-1">
+      <Card className="p-4 w-full  mx-auto space-y-4 bg-background">
+        <div className="flex sm:flex-row flex-col">
+          {/* Difficulty Filter */}
+          <div className="w-full mb-4">
+            <h3 className="text-lg font-semibold">Difficulty</h3>
+            <div className="flex gap-3 mt-2">
+              {difficulty.map((diff) => (
+                <label key={diff} className="flex items-center gap-2">
+                  <Checkbox
+                    checked={selectedDifficulty.includes(diff)}
+                    onCheckedChange={() => handleDifficultyChange(diff)}
+                  />
+                  {diff}
+                </label>
               ))}
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-      {/* Duration Filter */}
-      <div className="flex sm:flex-row flex-col gap-3">
-        <div className="w-full">
-          <h3 className="text-lg font-semibold ">Duration (days)</h3>
-          <Slider
-            min={1}
-            max={30}
-            step={1}
-            value={duration}
-            onValueChange={(value) => setDuration(value as [number, number])}
-            className="w-full [&>span]:w-4 [&>span]:h-4 [&>span]:bg-white [&>span]:border-2 [&>span]:border-violet-500 rounded-full"
-          />
-          <div className="text-sm mt-2">
-            {duration[0]} - {duration[1]} days
+            </div>
+          </div>
+          {/* Sort Filter */}
+          <div className="w-full">
+            <h3 className="text-lg font-semibold mb-2">Sort by</h3>
+            <Select value={sort} onValueChange={setSort}>
+              <SelectTrigger className="w-full">
+                {sortOptions.find((s) => s.value === sort)?.label}
+              </SelectTrigger>
+              <SelectContent>
+                {sortOptions.map((option) => (
+                  <SelectItem key={option.value} value={option.value}>
+                    {option.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
           </div>
         </div>
+        {/* Duration Filter */}
+        <div className="flex sm:flex-row flex-col gap-3">
+          <div className="w-full">
+            <h3 className="text-lg font-semibold mb-1">Duration (days)</h3>
+            <Slider
+              min={1}
+              max={30}
+              step={1}
+              value={duration}
+              onValueChange={(value) => setDuration(value as [number, number])}
+              className="w-full [&>span]:w-4 [&>span]:h-4 [&>span]:bg-white [&>span]:border-2 [&>span]:border-violet-500 rounded-full"
+            />
+            <div className="text-sm mt-2">
+              {duration[0]} - {duration[1]} days
+            </div>
+          </div>
 
-        {/* Price Filter */}
-        <div className="w-full">
-          <h3 className="text-lg font-semibold">Price ($)</h3>
-          <Slider
-            min={200}
-            max={5000}
-            step={50}
-            value={price}
-            onValueChange={(value) => setPrice(value as [number, number])}
-            className="w-full [&>span]:w-4 [&>span]:h-4 [&>span]:bg-white [&>span]:border-2 [&>span]:border-violet-500 rounded-full"
-          />
-          <div className="text-sm mt-2">
-            ${price[0]} - ${price[1]}
+          {/* Price Filter */}
+          <div className="w-full">
+            <h3 className="text-lg font-semibold mb-1">Price ($)</h3>
+            <Slider
+              min={200}
+              max={5000}
+              step={50}
+              value={price}
+              onValueChange={(value) => setPrice(value as [number, number])}
+              className="w-full [&>span]:w-4 [&>span]:h-4 [&>span]:bg-white [&>span]:border-2 [&>span]:border-violet-500 rounded-full"
+            />
+            <div className="text-sm mt-2">
+              ${price[0]} - ${price[1]}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex gap-3 px-6">
-        <Button className="w-full mt-4" onClick={handleApplyFilter}>
-          Apply Filters
-        </Button>
-        <Button className="w-full mt-4" onClick={handleClearFilter}>
-          Clear All Filters
-        </Button>
-      </div>
-    </Card>
+        <div className="flex gap-3 px-6">
+          <Button className="w-full mt-4" onClick={handleApplyFilter}>
+            Apply Filters
+          </Button>
+          <Button className="w-full mt-4" onClick={handleClearFilter}>
+            Clear All Filters
+          </Button>
+        </div>
+      </Card>
+    </div>
   );
 }

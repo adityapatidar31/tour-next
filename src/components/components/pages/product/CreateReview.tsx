bf9da6aa9316aa82d1c6153de758d64cdf38ecbd
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-05 01:29:37 +0530

    invalid the cache of the review

--- src/components/components/pages/product/CreateReview.tsx
+++ src/components/components/pages/product/CreateReview.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
@@ -18,17 +18,16 @@ import {
   useDeleteReview,
   useUpdateReview,
 } from "@/services/queryClient";
-import { useMutation, useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { Star } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
-import { Review } from "@/store/reviewSlice";
 import { SyncLoader } from "react-spinners";
 
 export default function Rating() {
   const [description, setDescription] = useState("");
   const [rating, setRating] = useState<number>();
   const [reviewId, setReviewId] = useState<string>("");
-
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const userId = useAppSelector((store) => store.user._id);
   const navigate = useNavigate();
   const { id: tourId } = useParams();
@@ -36,22 +35,6 @@ export default function Rating() {
     useUpdateReview();
   const { mutate: deleteMutate, isPending: isDeletePending } =
     useDeleteReview();
-
-  const { isLoading } = useQuery<Review | null, Error>({
-    queryKey: ["review"],
-    queryFn: async (): Promise<Review | null> => {
-      if (!userId || !tourId) return null;
-      return await findReviewByUserAndTour(tourId);
-    },
-    onSuccess: (data: Review | null) => {
-      if (data) {
-        setDescription(data.review);
-        setRating(data.rating);
-        setReviewId(data.id);
-      }
-    },
-    enabled: !!userId && !!tourId,
-  } as UseQueryOptions<Review | null, Error>);
 
   const mutation = useMutation({
     mutationFn: async () => {
@@ -65,19 +48,35 @@ export default function Rating() {
     },
     onSuccess: () => {
       toast.success("Review Created Successfully");
-      queryClient.invalidateQueries({ queryKey: ["tour", tourId] });
+      queryClient.invalidateQueries();
     },
     onError: (error) => {
       toast.error(error.message || "Failed to create review");
     },
   });
 
+  useEffect(
+    function () {
+      async function fetchReview() {
+        if (tourId && userId) {
+          setIsLoading(true);
+          const { id, review, rating } = await findReviewByUserAndTour(tourId);
+          if (review) setDescription(review);
+          if (rating) setRating(rating);
+          if (id) setReviewId(id);
+          setIsLoading(false);
+        }
+      }
+      fetchReview();
+    },
+    [tourId, userId]
+  );
+
   function handleUpdate() {
     if (!tourId || !description || !rating) {
       toast.error("Please fill in all required fields");
       return;
     }
-
     updateMutate({
       tourId,
       review: description,
@@ -85,66 +84,66 @@ export default function Rating() {
       reviewId,
     });
   }
-
   function handleDelete() {
-    if (!reviewId) return toast.error("Review ID is not found. Try again");
+    if (!reviewId) return toast.error("Review ID is not fount. Try again");
     deleteMutate(reviewId);
   }
-
-  if (!userId) return null;
-
+  if (!userId) return;
   return (
     <>
       <h2 className="text-2xl font-semibold flex items-center gap-3">
-        <Star className="w-6 h-6 text-violet-500" />
+        <span>
+          <Star className="w-6 h-6 text-violet-500" />
+        </span>
         Your review
       </h2>
       <Separator className="mt-2 mb-5" />
       <Card className="w-full max-w-full p-6 space-y-4">
         <CardContent className="space-y-4">
-          <>
-            <Select
-              onValueChange={(value) => setRating(Number(value))}
-              value={rating?.toString()}
-              required
-            >
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Select a rating" />
-              </SelectTrigger>
-              <SelectContent>
-                {[1, 2, 3, 4, 5].map((num) => (
-                  <SelectItem key={num} value={num.toString()}>
-                    {num}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <Textarea
-              placeholder="Enter your feedback"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="w-full"
-              required
-            />
-            {reviewId ? (
-              <div className="flex sm:gap-20 gap-10 justify-center">
-                <Button
-                  onClick={handleUpdate}
-                  disabled={isDeletePending || isUpdatePending}
-                  className="w-96"
-                >
-                  {isUpdatePending ? <SyncLoader color="#fff" /> : "Update"}
-                </Button>
-                <Button
-                  onClick={handleDelete}
-                  className="w-96"
-                  variant="destructive"
-                  disabled={isDeletePending || isUpdatePending}
-                >
-                  {isDeletePending ? <SyncLoader color="#fff" /> : "Delete"}
-                </Button>
-              </div>
-            ) : userId ? (
+          <Select
+            onValueChange={(value) => setRating(Number(value))}
+            value={rating?.toString()}
+            required
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select a rating" />
+            </SelectTrigger>
+            <SelectContent>
+              {[1, 2, 3, 4, 5].map((num) => (
+                <SelectItem key={num} value={num.toString()}>
+                  {num}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <Textarea
+            placeholder="Enter your feedback"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="w-full"
+            required
+          />
+          {reviewId && (
+            <div className="flex sm:gap-20 gap-10 justify-center">
+              <Button
+                onClick={handleUpdate}
+                disabled={isDeletePending || isUpdatePending}
+                className="w-96"
+              >
+                {isUpdatePending ? <SyncLoader color="#fff" /> : "Update"}
+              </Button>
+              <Button
+                onClick={handleDelete}
+                className="w-96"
+                variant="destructive"
+                disabled={isDeletePending || isUpdatePending}
+              >
+                {isDeletePending ? <SyncLoader color="#fff" /> : "Delete"}
+              </Button>
+            </div>
+          )}
+          {!reviewId &&
+            (userId ? (
               <Button
                 onClick={() => mutation.mutate()}
                 disabled={isLoading}
@@ -156,8 +155,7 @@ export default function Rating() {
               <Button onClick={() => navigate("/login")} className="w-full">
                 Login
               </Button>
-            )}
-          </>
+            ))}
         </CardContent>
       </Card>
     </>

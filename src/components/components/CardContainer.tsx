41a97cb576a4b9da8158df2250fa20e0641d5e2e
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-04 21:03:24 +0530

    add heading in the home page

--- src/components/components/CardContainer.tsx
+++ src/components/components/CardContainer.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import CardComponent from "./CardComponent";
 import { Tour } from "@/services/types";
+import { Separator } from "../ui/separator";
+import { MapPin } from "lucide-react";
 
 interface CardContainerProps {
   tours: Tour[];
@@ -8,24 +10,33 @@ interface CardContainerProps {
 
 function CardContainer({ tours }: CardContainerProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {tours.map((tour) => (
-        <Link to={`/home/${tour.id}`} key={tour.id}>
-          <CardComponent
-            category={tour.category}
-            name={tour.name}
-            duration={tour.duration}
-            maxGroupSize={tour.maxGroupSize}
-            difficulty={tour.difficulty}
-            ratingsAverage={tour.ratingsAverage}
-            ratingsQuantity={tour.ratingsQuantity}
-            price={tour.price}
-            summary={tour.summary}
-            imageCover={tour.imageCover}
-          />
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="flex items-center gap-2 text-2xl font-semibold m-3">
+        <MapPin className="w-6 h-6 text-violet-500" />
+        <span>Tours</span>
+      </div>
+      <Separator className="my-3" />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {tours.map((tour) => (
+          <Link to={`/home/${tour.id}`} key={tour.id}>
+            <CardComponent
+              category={tour.category}
+              name={tour.name}
+              duration={tour.duration}
+              maxGroupSize={tour.maxGroupSize}
+              difficulty={tour.difficulty}
+              ratingsAverage={tour.ratingsAverage}
+              ratingsQuantity={tour.ratingsQuantity}
+              price={tour.price}
+              summary={tour.summary}
+              imageCover={tour.imageCover}
+            />
+          </Link>
+        ))}
+      </div>
+      <Separator className="my-3 mb-10" />
+    </>
   );
 }
 

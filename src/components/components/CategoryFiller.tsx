ee6af2b8ce88b780fa4de00581117548234c4d09
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-04-26 23:46:00 +0530

    fix the bug ui of selected category

--- src/components/components/CategoryFiller.tsx
+++ src/components/components/CategoryFiller.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import {
   LucideIcon,
   Mountain,
@@ -31,24 +32,33 @@ const categories: { name: string; icon: LucideIcon }[] = [
 
 export default function CategoryFilter() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const activeCategory = searchParams.get("category");
 
   function addCategory(name: string) {
     searchParams.set("category", name);
     setSearchParams(searchParams);
   }
+
   return (
     <div className="overflow-x-auto w-full">
       <div className="flex gap-8 p-4 min-w-max">
-        {categories.map(({ name, icon: Icon }) => (
-          <div
-            key={name}
-            className="flex flex-col items-center p-1 gap-2 cursor-pointer"
-            onClick={() => addCategory(name)}
-          >
-            <Icon className="w-16 h-16 text-primary" />
-            <span className="text-m font-medium text-primary">{name}</span>
-          </div>
-        ))}
+        {categories.map(({ name, icon: Icon }) => {
+          const isActive = activeCategory === name;
+
+          return (
+            <div
+              key={name}
+              className={cn(
+                "flex flex-col items-center gap-2 p-2 cursor-pointer",
+                isActive && "bg-muted rounded-sm"
+              )}
+              onClick={() => addCategory(name)}
+            >
+              <Icon className="w-16 h-16 text-primary" />
+              <span className="text-m font-medium text-primary">{name}</span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
